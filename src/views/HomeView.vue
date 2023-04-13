24b3d623752c44a254480db5c64328ff8feb2b02
thanawat080 <thanawat.2543@yahoo.co.th>
2023-04-13 23:54:41 +0700

    test5

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,45 @@
 <template>
   <div class="container">
-    <div style="white-space: pre-wrap; text-align: left;">
-      <div class="cloumns">
-        <div class="cloumn">
-          {{ displayedText }}
-          <button class="button" @click="showFullText = !showFullText"> Read {{ readMoreText }}...</button>
+    <div class="columns">
+      <div class="column is-two-thirds">
+        <div style="white-space: pre-wrap; text-align: left; border: 1px solid; word-break: break-all; width: 680px;">
+          <div class="cloumns">
+            <div class="cloumn">
+              {{ text }}
+              <button class="button" @click="showFullText = !showFullText"> Read {{ readMoreText }}...</button>
+            </div>
+          </div>
+          <div class="cloumns">
+            <div class="cloumn">
+
+            </div>
+          </div>
         </div>
       </div>
-      <div class="cloumns">
-        <div class="cloumn">
-
+      <div class="column">
+        <div class="columns" >
+          <label for="" style="padding-bottom: 15px;">Select model for prediction</label>
+        </div>
+        <div class="columns">
+          <div class="select" style="margin-left: 25px;">
+          <select>
+            <option option>Classification</option>
+            <option>Regression</option>
+          </select>
+          <br>
+          <br>
+          <div class="button is-success" @click="tograph">submit</div>
+        </div>
         </div>
       </div>
     </div>
+
     <div class="modal" :style="{ 'display': modalstatus }">
       <div class="modal-background"></div>
       <div class="modal-card">
         <header class="modal-card-head">
           <p class="modal-card-title">Create post</p>
-          <button class="delete" aria-label="close"></button>
+          <button class="delete" aria-label="close" @click="modalstatus = 'none'"></button>
         </header>
         <section class="modal-card-body">
           <textarea class="textarea" placeholder="e.g. Hello world" v-model="text"></textarea>
@@ -26,9 +47,11 @@
           <div class="input">Add to your post
             <div class="image-upload" style="margin-left: 250px;">
               <label for="file-input">
-                <img src="https://static.xx.fbcdn.net/rsrc.php/v3/yC/r/a6OjkIIE-R0.png?_nc_eui2=AeEROHNgEIeAgmIkEz0Cir2CfK5Z1qDG7FV8rlnWoMbsVRBub7G9sWD6iVVh0J5TdGt_1i-OnVJ_PSthRF33l8_A" style=" height: 24px; cursor: pointer;"/>
+                <img
+                  src="https://static.xx.fbcdn.net/rsrc.php/v3/yC/r/a6OjkIIE-R0.png?_nc_eui2=AeEROHNgEIeAgmIkEz0Cir2CfK5Z1qDG7FV8rlnWoMbsVRBub7G9sWD6iVVh0J5TdGt_1i-OnVJ_PSthRF33l8_A"
+                  style=" height: 24px; cursor: pointer;" />
               </label>
-              <input id="file-input" type="file" style="display: none" v-on:change="onFileChange"/>
+              <input id="file-input" type="file" style="display: none" v-on:change="onFileChange" />
             </div>
             <img
               src="https://static.xx.fbcdn.net/rsrc.php/v3/yC/r/MqTJr_DM3Jg.png?_nc_eui2=AeFJI-Af9QMlz4__VIPfsowRt8fz8ZW9Ebm3x_Pxlb0RuY34momAG_iIJru00iVh9P572xJfSkS6MOm0sIxYoBQF"
@@ -64,6 +87,9 @@ export default {
     }
   },
   methods: {
+    tograph () {
+      this.$router.push({ name: 'graph' })
+    },
     onFileChange (e) {
       var files = e.target.files || e.dataTransfer.files
       console.log(files)
@@ -95,11 +121,11 @@ export default {
 
 <style>
 #truncate {
-   overflow: hidden;
-   text-overflow: "see more";
-   display: -webkit-box;
-   -webkit-line-clamp: 2; /* number of lines to show */
-           line-clamp: 2;
-   -webkit-box-orient: vertical;
-}
-</style>
+  overflow: hidden;
+  text-overflow: "see more";
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  /* number of lines to show */
+  line-clamp: 2;
+  -webkit-box-orient: vertical;
+}</style>
