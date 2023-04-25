6354ee4523c505e7a6c5a5f81b243edf1fcac7cd
thanawat <thanawat.2543@yahoo.co.th>
2023-04-25 21:47:39 +0700

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container" id="app">
     <div class="columns">
       <div class="column is-two-thirds">
         <div style="white-space: pre-wrap; text-align: left; border: 1px solid; word-break: break-all; width: 680px;">
@@ -82,6 +82,41 @@
             <img
               src="https://static.xx.fbcdn.net/rsrc.php/v3/yY/r/CenxFlWjtJO.png?_nc_eui2=AeHNEdymbgD-8uBWR6mmjZ8Kz4XXObU63WDPhdc5tTrdYOq3gKzMywwqCKKeW8fTJ_YGuqtay6hQSitG7v1S5BGz"
               style="margin-left: 15px; height: 24px;" />
+            <div class="wrapper">
+                <emoji-picker @emoji="append" :search="search">
+                  <button
+                    class="emoji-invoker"
+                    slot="emoji-invoker"
+                    slot-scope="{ events: { click: clickEvent } }"
+                    @click.stop="clickEvent"
+                  >
+                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 fill-current text-grey">
+                        <path d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z"/>
+                    </svg>
+                  </button>
+                  <div slot="emoji-picker" slot-scope="{ emojis, insert, display }">
+                    <div class="emoji-picker" :style="{ top: display.y + 'px', left: display.x + 'px' }">
+                      <div class="emoji-picker__search">
+                        <input type="text" v-model="search" v-focus>
+                      </div>
+                      <div>
+                        <div v-for="(emojiGroup, category) in emojis" :key="category">
+                          <h5>{{ category }}</h5>
+                          <div class="emojis">
+                            <span
+                              v-for="(emoji, emojiName) in emojiGroup"
+                              :key="emojiName"
+                              @click="insert(emoji)"
+                              :title="emojiName"
+                            >{{ emoji }}</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </emoji-picker>
+            </div>
           </div>
         </section>
         <footer class="modal-card-foot">
@@ -94,9 +129,15 @@
 </template>
 
 <script>
+import EmojiPicker from 'vue-emoji-picker'
 export default {
+  components: {
+    EmojiPicker
+  },
   data () {
     return {
+      input: '',
+      search: '',
       createImage: '',
       showFullText: false,
       text: '',
@@ -105,6 +146,9 @@ export default {
     }
   },
   methods: {
+    append (emoji) {
+      this.text += emoji
+    },
     tograph () {
       this.$router.push({ name: 'graph' })
     },
@@ -120,6 +164,13 @@ export default {
     submitpost () {
       this.modalstatus = 'none'
       console.log(this.text)
+    }
+  },
+  directives: {
+    focus: {
+      inserted (el) {
+        el.focus()
+      }
     }
   },
   computed: {
@@ -157,4 +208,88 @@ export default {
  background:#eee;
  cursor:pointer;
 }
+/* Tailwind CSS-styled demo is available here: https://codepen.io/DCzajkowski/pen/Brxvzj */
+.regular-input {
+  padding: 0.5rem 1rem;
+  border-radius: 3px;
+  border: 1px solid #ccc;
+  width: 20rem;
+  height: 12rem;
+  outline: none;
+}
+
+.regular-input:focus {
+  box-shadow: 0 0 0 3px rgba(66,153,225,.5);
+}
+
+.emoji-invoker {
+  position: absolute;
+  top: 0.5rem;
+  right: 0.5rem;
+  width: 1.5rem;
+  height: 1.5rem;
+  border-radius: 50%;
+  cursor: pointer;
+  transition: all 0.2s;
+  padding: 0;
+  background: transparent;
+  border: 0;
+}
+.emoji-invoker:hover {
+  transform: scale(1.1);
+}
+.emoji-invoker > svg {
+  fill: #b1c6d0;
+}
+
+.emoji-picker {
+  position: fixed;
+  z-index: 1;
+  font-family: Montserrat;
+  border: 1px solid #ccc;
+  width: 15rem;
+  height: 20rem;
+  overflow: scroll;
+  padding: 1rem;
+  box-sizing: border-box;
+  border-radius: 0.5rem;
+  background: #fff;
+  box-shadow: 1px 1px 8px #c7dbe6;
+}
+.emoji-picker__search {
+  display: flex;
+}
+.emoji-picker__search > input {
+  flex: 1;
+  border-radius: 10rem;
+  border: 1px solid #ccc;
+  padding: 0.5rem 1rem;
+  outline: none;
+}
+.emoji-picker h5 {
+  margin-bottom: 0;
+  color: #b1b1b1;
+  text-transform: uppercase;
+  font-size: 0.8rem;
+  cursor: default;
+}
+.emoji-picker .emojis {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+.emoji-picker .emojis:after {
+  content: "";
+  flex: auto;
+}
+.emoji-picker .emojis span {
+  padding: 0.2rem;
+  cursor: pointer;
+  border-radius: 5px;
+}
+.emoji-picker .emojis span:hover {
+  background: #ececec;
+  cursor: pointer;
+}
+
 </style>
